c5705a4b2a8d51f825088d9e625a9b1b65770700
Jorge <jorgeachaar@hotmail.com>
2017-05-19 10:51:24 -0300

    Fix Mask

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -9,38 +9,16 @@ class ImageController extends Controller
 {
 	public function index()
 	{
-		$marco = Image::make(public_path() . '/Krito/img/marco1.png');
-		// $img   = Image::make(public_path() . '/Krito/img/logo.png');
-		// $img2  = Image::make(public_path() . '/Krito/img/background/contact.jpg');
-		// $img->crop(250, 350);
-		// $marco->insert($img, 'center');
+		$marco = Image::make(public_path() . '/Krito/img/asd/marco.png');
+		$mask  = Image::make(public_path() . '/Krito/img/asd/mask.png');
+		$img   = Image::make(public_path() . '/Krito/img/logo.png');
+		$img->resize(585, 539);
 
-		// $thumbnail = $img->thumbnail(
-  //           $this->size,
-  //           Imagine\Image\ImageInterface::THUMBNAIL_OUTBOUND
-  //       );
 
-  //       return $thumbnail->response('png');
-		// return $marco->response('png');
-		// return "se crea imagen";
 
-		$img = Image::canvas(300, 200, '#ddd');
-
-		// draw a filled blue circle
-		$img->circle(100, 50, 50, function ($draw) {
-		    $draw->background('#0000ff');
-		});
-
-		// draw a filled blue circle with red border
-		$img->circle(10, 100, 100, function ($draw) use $marco {
-		    $draw->background($marco);
-		    $draw->border(1, '#f00');
-		});
-
-		// draw an empty circle with 5px border
-		$img->circle(70, 150, 100, function ($draw) {
-		    $draw->border(5, '000000');
-		});
+		$img->mask($mask, true);
+		$img->insert($marco, 'center');
+		//$marco->resizeCanvas(200, 200, 'center', false, array(255, 255, 255, 0));
 
 		return $img->response('png');
 	}
